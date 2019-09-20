62c5ab55851141d52cd79552b1d9a72f6582bf11
Lucas Fogaça <lucasfogaca@MacBook-Pro-de-Lucas.local>
2019-09-20 19:56:53 -0300

    First Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAgil.WebApi.Data;
+using ProAgil.WebApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProAgil.WebApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    public readonly DataContext _context;
+
+    public ValuesController(DataContext context)
+    {
+      _context = context;
+
+    }
+    // GET api/values
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+
+      try
+      {
+        var results = await _context.Eventos.ToListAsync();
+        return Ok(results);
+      }
+      catch (System.Exception)
+      {
+        return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar no banco");
+      }
+
+
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      try
+      {
+        var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+        return Ok(results);
+      }
+      catch (System.Exception)
+      {
+        return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar no banco");
+      }
+
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
